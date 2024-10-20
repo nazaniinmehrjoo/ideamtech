fa8235f15cfbbe62ab15d4d87e1b5ffa0560ff48
AmmirrezaHamidi <amirreza.hamidi138@gmail.com>
2024-10-20 09:02:12 +0330

    fixed app2.blade.php

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>
         @isset($title)
             {{ $title }} | 
@@ -12,8 +13,16 @@
     <!-- Stylesheets -->
     <link href="/assets/css/bootstrap.css" rel="stylesheet">
     <link href="/assets/css/style.css" rel="stylesheet">
-    <!-- <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
-    <link rel="icon" href="/assets/images/favicon.png" type="image/x-icon"> -->
+    <!-- <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon"> -->
+    <!-- <link rel="icon" href="/assets/images/favicon.png" type="image/x-icon"> -->
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css" rel="stylesheet">
+
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+
+    <!-- Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -37,7 +46,7 @@
         <div class="menu-backdrop"></div>
         
         <!-- Main Header -->
-        @include('partials.mainheader')
+        @include('partials.mainHeader')
 
         <div class="scroll-container">
             <div class="main-content-container" id="scroll-container">
@@ -62,5 +71,51 @@
 
     <!-- Laravel Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Click Tracking Function -->
+    <script>
+        function trackClick(productId) {
+            fetch(`/products/${productId}/click`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}' 
+                },
+                body: JSON.stringify({})
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); 
+            })
+            .then(data => {
+                console.log(data.message); 
+            })
+            .catch(error => {
+                console.error('Error tracking click:', error);
+            });
+        }
+    </script>
+    <script>
+    let pageLoadTime = performance.now(); 
+
+    // When the user leaves the page or closes the tab
+    window.addEventListener('beforeunload', function () {
+        let pageCloseTime = performance.now();
+        let timeSpent = (pageCloseTime - pageLoadTime) / 1000; // Time in seconds
+
+        // Send the time spent to the server using the Fetch API
+        fetch('/track-time-spent', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            },
+            body: JSON.stringify({ timeSpent: timeSpent }) 
+        });
+    });
+</script>
+
 </body>
 </html>
