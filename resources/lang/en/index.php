f8ebe28fe5fe5c850d619ea9dd437a82467c02dd
AmmirrezaHamidi <amirreza.hamidi138@gmail.com>
2024-11-27 11:56:57 +0330

    Add Languages for Index

---
+++ resources/lang/en/index.php
@@ -0,0 +1,72 @@
+<?php
+return [
+    'slider' => [
+        'slide_1' => [
+            'cat' => 'Dryer',
+            'title' => 'Faster production with optimized energy consumption',
+        ],
+        'slide_2' => [
+            'cat' => 'Robot',
+            'desc' => 'Fully mechanized production line',
+        ],
+        'slide_3' => [
+            'cat' => 'Iraqi Brick Dryer',
+            'desc' => 'The first rapid Iraqi brick dryer in the world',
+        ],
+        'slide_4' => [
+            'cat' => 'Tunnel Kiln',
+            'title' => 'Design and construction of tunnel kilns',
+        ],
+        'slide_5' => [
+            'cat' => 'About Borna Gostar',
+            'desc' => 'Ideas for design and construction',
+        ],
+
+    ],
+    // =============================================
+    'services' => [
+        'title' => 'Our Stories and Achievements',
+        'customer_satisfaction' => [
+            'title' => 'Customer Satisfaction',
+            'description' => 'Customer satisfaction is our priority. Borna Gostar team provides professional services and ongoing support to ensure a reliable and satisfying experience for you.',
+        ],
+        'news_and_events' => [
+            'title' => 'News and Events',
+            'description' => 'Stay updated with the latest news and events in the brick industry. Learn about the newest projects, developments, and achievements of our team in this field.',
+        ],
+        'distinct' => [
+            'title' => 'Distinct From Others',
+            'description' => 'We stand by you from the beginning to the end and beyond.',
+        ],
+        'simulator' => [
+            'title' => 'Simulator',
+            'description' => 'This device predicts the drying time of various bricks and tiles based on temperature, humidity, and airspeed.',
+        ],
+    ],
+    // ====================================================================
+    'services_two' => [
+        'subtitle' => 'Borna Gostar Path',
+        'title' => 'Insight, Values, and Achievements',
+        'tabs' => [
+            'about' => [
+                'title' => 'About Borna Gostar',
+                'content' => 'Since 2006, Borna Gostar Parsian has utilized skilled experts and modern knowledge to localize technical knowledge and optimize production line equipment for bricks and similar products. Our expertise spans consultancy, factory establishment, production line optimization, and more, with unique products and patents in rotary dryers, robots, and related machinery.',
+                'button_text' => 'Join the Borna Gostar Family',
+                'button_link' => 'join-borna-gostar-family',
+            ],
+            'catalog' => [
+                'title' => 'Catalog',
+                'content' => 'Download our comprehensive catalog to explore Borna Gostar’s products and services. The catalog contains detailed information on our innovative solutions, technical specifications, and unique features to help you make the best choices for your projects.',
+                'download_text' => 'Download Catalog',
+                'download_link' => '/assets/catalog/catalog.pdf',
+            ],
+            'values' => [
+                'title' => 'Values and Mission',
+                'content' => [
+                    'goals' => 'Our goal is to revive the brick industry in the country and create distinctive products.',
+                    'beliefs' => 'We believe in honesty, integrity, and respect as organizational values. Our profits come from fostering enthusiasm in convergent communities. Innovation is woven into the fabric of our products and services, paving the way to hope.',
+                ],
+            ],
+        ],
+    ],
+];
