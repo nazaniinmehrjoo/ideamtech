acc3b602c6d338e1d7e6406681715179f84b67bb
AmmirrezaHamidi <amirreza.hamidi138@gmail.com>
2024-12-06 23:34:13 +0330

    remove somde migration base I nedede:)

--- database/migrations/2024_10_02_110013_create_products_table.php
+++ database/migrations/2024_10_02_110013_create_products_table.php
@@ -11,23 +11,11 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->json('name'); 
             $table->unsignedBigInteger('category_id');
-            $table->json('description')->nullable(); // Change to JSON for multilingual support
+            $table->json('description')->nullable(); 
             $table->string('image')->nullable();
             $table->string('page_name');
             $table->integer('views')->default(0);
             $table->integer('clicks')->default(0);
-            
-            // Add fields for chart metrics
-            $table->integer('total_cost')->nullable();
-            $table->integer('energy_consumption')->nullable();
-            $table->integer('production_variety')->nullable();
-            $table->integer('occupied_area')->nullable();
-            $table->integer('drying_time')->nullable();
-            $table->integer('maintenance_cost')->nullable();
-            $table->integer('product_quality')->nullable();
-            $table->integer('operation_cost')->nullable();
-            $table->integer('machine_quality')->nullable();
-            
             $table->timestamps();
         });
     }
