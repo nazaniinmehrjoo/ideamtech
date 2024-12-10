12f9e176976c10595a47846298df86937514589c
NazaninMehrjoo <nazaninmehrjoou@gmail.com>
2024-12-10 12:24:09 +0330

    Change bornagostar logo in header of site

--- resources/views/partials/mainHeader.blade.php
+++ resources/views/partials/mainHeader.blade.php
@@ -1,9 +1,8 @@
 <!-- Main Header Bar-->
 <!--Alt Logo Box-->
 
-
 <div class="alt-logo-box">
-    <a href="/"><img src="/assets/images/logotest2.png" alt="{{ __('header.alt_logo') }}"></a>
+    <a href="/"><img src="/assets/images/bornagostar.png" alt="{{ __('header.alt_logo') }}"></a>
 </div>
 
 <header class="main-header-bar">
