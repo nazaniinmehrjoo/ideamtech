71ebd97d6e2e05033e368b4cee10d4d6831f8659
AmirrezaHamidi <amirreza.hamidi138@gmail.com>
2024-12-16 12:14:25 +0330

    Localized page Simolator

---
+++ resources/lang/en/simulator.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'title' => 'Simulator',
+    'slide_1_title' => 'According to soil quality and regional climate conditions',
+    'slide_2_title' => 'Precise control over temperature, humidity, and airflow speed',
+    'slide_3_title' => 'Design and manufacture of the first dryer simulator in the brick industry',
+    'pager_1_text' => 'Estimate drying time of wet bricks with the highest quality',
+    'pager_2_text' => 'Calculate the dryer dimensions according to the factory\'s required capacity',
+    'pager_3_text' => 'Troubleshooting of the produced tiles',
+];
