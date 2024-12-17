0b52c3161c09006d13d670ca24c17f72b9d5fb4f
NazaninMehrjoo <nazaninmehrjoou@gmail.com>
2024-12-17 13:37:52 +0330

    Fix 'درباره برنا' page Translation

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -44,23 +44,26 @@ return [
         'title' => 'Borna Gostar\'s achievements and values',
         'tabs' => [
             'tab-1' => [
-                'title' => 'Catalog',
+                'title' => 'catalogue',
                 'image' => '/assets/images/resource/catalog.png',
-                'description' => 'Download our comprehensive catalog to learn more about our products and services. This catalog provides detailed information about our production, technical specifications, and innovative solutions.',
+                'description' => 'For more information about our company\'s products and services, kindly download the comprehensive catalogue. In this catalogue, you can find complete details about our products, product specifications and innovative solutions that we offer.',
                 'button' => true,
-                'button_text' => 'Download Catalog',
+                'button_text' => 'Download catalogue',
                 'button_link' => '/assets/catalog/catalog.pdf',
             ],
             'tab-2' => [
-                'title' => 'Technical Approval',
+                'title' => 'Technical approval',
                 'image' => '/assets/images/resource/taeede.jpg',
-                'description' => 'Obtaining technical approvals and certificates of completion ensures adherence to all quality and technical standards throughout the project execution stages.',
+                'description' => 'Obtaining technical approval and certificate of good performance after project delivery means ensuring full compliance with quality and technical standards in all phases of project implementation. This confirmation indicates that the project has been implemented in accordance with the technical specifications and requirements set by the employer and that all phases of design, construction, installation and operation have been carried out correctly and without defects.',
                 'button' => false,
             ],
             'tab-3' => [
-                'title' => 'Mission and Values',
+                'title' => 'Values and mission',
                 'image' => '/assets/images/main-slider/pokht.jpg',
-                'description' => 'Our goal is to revitalize the brick industry in the country and create distinctive products. We believe in honesty, integrity, and respect as guiding principles for our organization, managers, and employees.',
+                'description' =>
+
+                    'Our goal is to revive the brick industry in the country and create distinctive products. 
+                    Truth, honesty and respect are the current character of our organizational values. We seek our profit in increasing business energy. Innovation is in warp and woof of our products and services.',
                 'button' => false,
             ],
         ],
@@ -70,26 +73,26 @@ return [
                 'icon' => 'fa-regular fa-trophy',
                 'items' => [
                     'Acquiring technical knowledge of all kind of dryer types.',
-                    'Acquiring  technical knowledge of manufacturing of all kind of industrial robots.',
-                    'Acquiring technical knowledge and manufacturing an axial fan with the highest efficiency for use in the dryer tunnel ',
-                    'Modeling brick manufacturing factories before construction.',
-                    'Providing design, consulting, and optimization for brick production lines.',
+                    'Acquiring  technical knowledge of manufacturing of all kind of industrial robots .',
+                    'Acquiring technical knowledge and manufacturing an axial fan with the highest efficiency for use in the dryer tunnel.',
+                    'Acquiring technical knowledge of modeling brick factories before installation.',
+                    'Acquiring technical ability in designing, consulting and optimizing the construction of clay brick production lines (TURNKEY PROJECTS).',
                 ],
             ],
             [
-                'title' => 'Execution Capability',
+                'title' => 'Execution Ability',
                 'icon' => 'fa-light fa-user-helmet-safety',
                 'items' => [
-                    'Building advanced dryers with the latest technology.',
-                    'Completing several brick production projects in Iran, Azerbaijan, and Iraq.',
+                    'Manufacturing all kinds of dryers with the latest technology and reducing costs compared to chamber dryers.',
+                    'Carrying out 10 installation projects and setting up a complete brick production line in Iran, Azerbaijan and Iraq (turnkey projects)',
                 ],
             ],
             [
-                'title' => 'Support and Spare Parts',
+                'title' => 'Support and spare parts supply',
                 'icon' => 'fa-regular fa-user-headset',
                 'items' => [
-                    'Contracts for maintaining and repairing brick production lines.',
-                    'Providing technical support and spare parts for brick production lines.',
+                    'Contract closure for modification, repair and maintenance of production lines of brick plants.',
+                    'Ability to provide technical services and supply all spare parts required for brick production lines.',
                 ],
             ],
         ],
